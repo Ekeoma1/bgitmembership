794e9f8537aab80822738364e0f430f8bc8bc6bc
ekeoma1 <ekeigwee@gmail.com>
2023-08-12 07:12:15 +0100

    home page, dashboard and other users

---
+++ src/components/Modals/ReportModal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/scss/modal.scss";
+
+const ReportModal = () => {
+  return (
+    <div className="report-modal shadow">
+      <div className="report-modal-content"></div>
+    </div>
+  );
+};
+
+export default ReportModal;
