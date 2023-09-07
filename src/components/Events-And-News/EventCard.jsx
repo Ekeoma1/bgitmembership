8dae20a581e56553376e74976b901d43e1bd99f1
ekeoma1 <ekeigwee@gmail.com>
2023-09-07 09:06:28 +0100

    updated the event page

--- src/components/Events-And-News/EventCard.jsx
+++ src/components/Events-And-News/EventCard.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import { BsBookmark } from 'react-icons/bs';
-import { PiShareFat } from 'react-icons/pi';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { BsBookmark } from "react-icons/bs";
+import { PiShareFat } from "react-icons/pi";
+import { useNavigate } from "react-router-dom";
 
 const EventCard = ({ event }) => {
   const navigate = useNavigate();
   return (
-    <div
-      className='event-card'
-      onClick={() => navigate('/events-and-news/event')}
-    >
-      <div className='card-main'>
-        <div className='card-info'>
+    <div className="event-card shadow" onClick={() => navigate("/events-and-news/event")}>
+      <div className="card-main">
+        <div className="card-info">
           <h2>{event.title}</h2>
-          <p className='date'>{event.date}</p>
-          {event.location && <p className='location'>{event.location}</p>}
-          <p className='info'>{event.info}</p>
+          <p className="date">{event.date}</p>
+          {event.location && <p className="location">{event.location}</p>}
+          <p className="info">{event.info}</p>
         </div>
-        <div className='card-img'>
-          <img src={event.img} alt='event-people' className='img-card' />
+        <div className="card-img">
+          <img src={event.img} alt="event-people" className="img-card" />
         </div>
       </div>
-      <div className='card-footer'>
-        <div className='icon-wrapper'>
-          <BsBookmark className='icon' />
+      <div className="card-footer">
+        <div className="icon-wrapper">
+          <BsBookmark className="icon" />
           <p>{event.bookmark}</p>
         </div>
-        <div className='icon-wrapper'>
-          <PiShareFat className='icon' />
+        <div className="icon-wrapper">
+          <PiShareFat className="icon" />
           <p>{event.share}</p>
         </div>
       </div>
--- src/components/Events-And-News/LatestNews.jsx
+++ src/components/Events-And-News/LatestNews.jsx
@@ -1,144 +1,124 @@
-import React, { useEffect, useRef, useState } from 'react';
-import SearchBox from '../Molecules/SearchBox';
-import { VscSettings } from 'react-icons/vsc';
-import { HiChevronDown } from 'react-icons/hi';
-import { BsBookmark } from 'react-icons/bs';
-import { PiShareFat } from 'react-icons/pi';
-import people1 from '../../assets/images/event-people1.svg';
-import people2 from '../../assets/images/event-people2.svg';
-import { Scrollbar } from 'react-scrollbars-custom';
-import EventCard from './EventCard';
+import React, { useEffect, useRef, useState } from "react";
+import SearchBox from "../Molecules/SearchBox";
+import { VscSettings } from "react-icons/vsc";
+import { HiChevronDown } from "react-icons/hi";
+import { BsBookmark } from "react-icons/bs";
+import { PiShareFat } from "react-icons/pi";
+import people1 from "../../assets/images/event-people1.svg";
+import people2 from "../../assets/images/event-people2.svg";
+import { Scrollbar } from "react-scrollbars-custom";
+import EventCard from "./EventCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const LatestNews = () => {
   const data = [
     {
-      title: 'BGIT Summer Mixer',
-      date: '21 July 2023',
-      info: 'We want to express our heartfelt appreciation and share the immense joy we experienced during the Community Mixer held on... ',
+      title: "BGIT Summer Mixer",
+      date: "21 July 2023",
+      info: "We want to express our heartfelt appreciation and share the immense joy we experienced during the Community Mixer held on... ",
       img: people1,
-      bookmark: '2',
-      share: '7',
+      bookmark: "2",
+      share: "7",
     },
     {
-      title: 'BGIT x Showcode - Athena Hackathon',
-      date: '21 July 2023',
-      info: 'We are thrilled to share the remarkable success of our recent partnership with Showcode...',
+      title: "BGIT x Showcode - Athena Hackathon",
+      date: "21 July 2023",
+      info: "We are thrilled to share the remarkable success of our recent partnership with Showcode...",
       img: people2,
-      bookmark: '5',
-      share: '8',
+      bookmark: "5",
+      share: "8",
     },
     {
-      title: 'BGIT x Showcode - Athena Hackathon',
-      date: '21 July 2023',
-      info: 'We are thrilled to share the remarkable success of our recent partnership with Showcode...',
+      title: "BGIT x Showcode - Athena Hackathon",
+      date: "21 July 2023",
+      info: "We are thrilled to share the remarkable success of our recent partnership with Showcode...",
       img: people2,
-      bookmark: '5',
-      share: '8',
+      bookmark: "5",
+      share: "8",
     },
     {
-      title: 'BGIT x Showcode - Athena Hackathon',
-      date: '21 July 2023',
-      info: 'We are thrilled to share the remarkable success of our recent partnership with Showcode...',
+      title: "BGIT x Showcode - Athena Hackathon",
+      date: "21 July 2023",
+      info: "We are thrilled to share the remarkable success of our recent partnership with Showcode...",
       img: people2,
-      bookmark: '5',
-      share: '8',
+      bookmark: "5",
+      share: "8",
     },
     {
-      title: 'BGIT x Showcode - Athena Hackathon',
-      date: '21 July 2023',
+      title: "BGIT x Showcode - Athena Hackathon",
+      date: "21 July 2023",
 
-      info: 'We are thrilled to share the remarkable success of our recent partnership with Showcode...',
+      info: "We are thrilled to share the remarkable success of our recent partnership with Showcode...",
       img: people2,
-      bookmark: '5',
-      share: '8',
+      bookmark: "5",
+      share: "8",
     },
   ];
-  const elementRef = useRef(null);
-  const scrollBarConRef = useRef(null);
-  const scrollBarRef = useRef(null);
-  const [scrollbarWidth, setScrollbarWidth] = useState(0);
-  // const getScrollBarWidth = () => {
-  //   if (elementRef.current && scrollBarConRef.current && scrollBarRef.current) {
-  //     const element = elementRef.current;
-
-  //     const scrollLeft = element.scrollLeft;
-  //     const cardsWrapperFullWidth = element.scrollWidth;
-  //     const cardsWrapperVisibleWidth = element.offsetWidth;
-  //     const scrollWidth = cardsWrapperFullWidth - cardsWrapperVisibleWidth;
-  //     console.log('scroll width', scrollWidth);
-
-  //     const scrollBarConWidth = scrollBarConRef.current.offsetWidth;
-  //     const scrollbarWidth = scrollBarRef.current.offsetWidth;
-  //     const availableSpace = scrollBarConWidth - scrollbarWidth;
-  //     const scrollbarWidthPercentage =
-  //       (scrollbarWidth / scrollBarConWidth) * 100;
-
-  //     const x = (scrollBarConWidth * scrollLeft) / scrollWidth;
-  //     const y = (100 * x) / scrollBarConWidth;
-  //     const z = y - scrollbarWidthPercentage;
-  //     let marginLeft;
-  //     if (z < 0) {
-  //       marginLeft = 0;
-  //     } else {
-  //       marginLeft = z;
-  //     }
-  //     setScrollbarWidth(marginLeft);
-  //     console.log('scroll bar width temp', scrollLeft);
-  //   }
-  // };
-  useEffect(() => {}, []);
-  console.log('bottom', scrollBarConRef.current?.offsetWidth);
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 2,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  // const elementRef = useRef(null);
+  // const scrollBarConRef = useRef(null);
+  // const scrollBarRef = useRef(null);
+  // const [scrollbarWidth, setScrollbarWidth] = useState(0);
+  // useEffect(() => {}, []);
   return (
-    <div className='latest-news-wrapper'>
-      <div className='latest-news-content'>
-        <div className='section-header'>
-          <h5>Latest news</h5>
-          <div className='search-wrapper'>
-            <div className='search-box-wrapper'>
-              <div className='search-box-con'>
-                <SearchBox placeholder={'Search news or events'} />
+    <div className="latest-news-wrapper">
+      <div className="container">
+        <div className="latest-news-content">
+          <div className="section-header row align-items-end">
+            <div className="col-md">
+              <h2>Latest news</h2>
+            </div>
+            <div className="col-md">
+              <div className="search-wrapper">
+                <div className="search-box-wrapper">
+                  <div className="search-box-con">
+                    <SearchBox placeholder={"Search news or events"} />
+                  </div>
+                </div>
+                <div className="filter-wrapper shadow">
+                  <div className="filter">
+                    <VscSettings />
+                    <p>Filter</p>
+                  </div>
+                  <div className="sort">
+                    <p>SORT BY: LATEST</p>
+                    <HiChevronDown />
+                  </div>
+                </div>
               </div>
             </div>
-            <div className='filter-wrapper'>
-              <div className='filter'>
-                <VscSettings />
-                <p>Filter</p>
-              </div>
-              <div className='sort'>
-                <p>SORT BY: LATEST</p>
-                <HiChevronDown />
-              </div>
+          </div>
+
+          <div className="section-content">
+            <div className="cards-wrapper">
+              <Carousel responsive={responsive}>
+                {data.map((event, index) => (
+                  <EventCard event={event} key={index} />
+                ))}
+              </Carousel>
+            </div>
+          </div>
+
+          <div className="section-bottom">
+            <div className="see-all">
+              <h5 className="">See all</h5>
             </div>
           </div>
         </div>
-        <div className='section-content'>
-          <div
-            ref={elementRef}
-            className='cards-wrapper'
-            onScroll={() => {
-              // getScrollBarWidth();
-            }}
-          >
-            {data.map((event, index) => (
-              <EventCard event={event} key={index} />
-            ))}
-          </div>
-        </div>
-        <div className='section-bottom'>
-          <div className='scroll-bar-wrapper' ref={scrollBarConRef}>
-            <div
-              ref={scrollBarRef}
-              style={{ marginLeft: `${scrollbarWidth}%` }}
-              className='scroll-bar'
-            ></div>
-          </div>
-          <div className='see-all'>
-            <h5 className=''>See all</h5>
-          </div>
-        </div>
-        {/* <Scrollbar style={{ width: 250, height: 250 }}>
-          <p>Hello world!</p>
-        </Scrollbar> */}
       </div>
     </div>
   );
