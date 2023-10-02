95b9b34b3e2ec6b0ab43cdde1afcd81cb715fc98
ekeoma1 <ekeigwee@gmail.com>
2023-10-02 08:29:11 +0100

    update on home page

--- src/pages/ViewOtherUsers.jsx
+++ src/pages/ViewOtherUsers.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import Icon from '../components/Icon';
-import '../assets/scss/dashboard.scss';
-import ProfileBanner from '../components/Dashboard/ProfileBanner';
-import BioCard from '../components/Dashboard/BioCard';
-import SocialLinksCard from '../components/Dashboard/SocialLinksCard';
-import Group from '../components/Dashboard/Group';
-import Posts from '../components/Dashboard/Posts';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Icon from "../components/Icon";
+import "../assets/scss/dashboard.scss";
+import ProfileBanner from "../components/Dashboard/ProfileBanner";
+import BioCard from "../components/Dashboard/BioCard";
+import SocialLinksCard from "../components/Dashboard/SocialLinksCard";
+import Group from "../components/Dashboard/Group";
+import Posts from "../components/Dashboard/Posts";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ViewOtherUsers = () => {
+  const param = useParams();
+  console.log(param);
   const Navigate = useNavigate();
   return (
-    <div className='user-dashboard'>
-      <div className='container'>
+    <div className="user-dashboard">
+      <div className="container">
         <div>
           <button onClick={() => Navigate(-1)}>
-            <Icon icon='arrowLeft' />
+            <Icon icon="arrowLeft" />
           </button>
         </div>
 
-        <div className='row mt-5'>
-          <div className='col-lg-9 col-12'>
+        <div className="row mt-5">
+          <div className="col-lg-9 col-12">
             <ProfileBanner othersView={true} />
             <BioCard othersView={true} />
             <Posts />
