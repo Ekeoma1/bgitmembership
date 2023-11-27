d4da51c3c90d98579058d7ef82c2fcd0e2326437
ekeoma1 <ekeigwee@gmail.com>
2023-11-27 04:27:57 +0100

    update on mobile nav

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -2,25 +2,39 @@ import React from "react";
 import "./../assets/scss/navFooter.scss";
 import Icon from "./Icon";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const MobileNav = ({ hide, nav }) => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   return (
     <div className={`mobile-nav shadow d-lg-none ${!nav && "d-none"}`}>
       <button onClick={hide}>
         <Icon icon="close" />
       </button>
-      <div className=" text-center">
-        <div className="mobile-menu-item">
-          <Link to="#">My Profile</Link>
+      {isLoggedIn ? (
+        <div className=" text-center">
+          <div onClick={hide} className="mobile-menu-item">
+            <Link to="#">My Profile</Link>
+          </div>
+          <div onClick={hide} className="mobile-menu-item">
+            <Link to="/events-and-news">News & Events</Link>
+          </div>
+          <div onClick={hide} className="mobile-menu-item">
+            <Link to="/settings">Settings</Link>
+          </div>
+          <button className="mobile-nav-log-out-btn">Log Out</button>
         </div>
-        <div className="mobile-menu-item">
-          <Link to="#">News & Events</Link>
+      ) : (
+        <div className=" text-center">
+          <div onClick={hide} className="mobile-menu-item">
+            <Link to="/login">Login / Register</Link>
+          </div>
+          <div onClick={hide} className="mobile-menu-item">
+            <Link to="#">Contact Us</Link>
+          </div>
         </div>
-        <div className="mobile-menu-item">
-          <Link to="#">Settings</Link>
-        </div>
-        <button className="mobile-nav-log-out-btn">Log Out</button>
-      </div>
+      )}
     </div>
   );
 };
--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,55 +1,52 @@
-import { useState, useEffect } from 'react';
-import '../../src/assets/scss/event.scss';
-import Banner from '../components/Event/Banner';
-import EventDetails from '../components/Event/EventDetails';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { triggerGetEventByID } from '../Features/events/events_slice';
-import { triggerGetNewsByID } from '../Features/news/news_slice';
-import Banner2 from '../components/Event/Banner2';
-import EventDetails2 from '../components/Event/EventDetails2';
+import { useState, useEffect } from "react";
+import "../../src/assets/scss/event.scss";
+import Banner from "../components/Event/Banner";
+import EventDetails from "../components/Event/EventDetails";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { triggerGetEventByID } from "../Features/events/events_slice";
+import { triggerGetNewsByID } from "../Features/news/news_slice";
+import Banner2 from "../components/Event/Banner2";
+import EventDetails2 from "../components/Event/EventDetails2";
 
 const Event = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const [data, setData] = useState({});
-  const [dataType, setDataType] = useState('');
+  const [dataType, setDataType] = useState("");
   const { getEventById } = useSelector((state) => state.events);
   const { getNewsById } = useSelector((state) => state.news);
 
-  const [tab, setTab] = useState('about');
+  const [tab, setTab] = useState("about");
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   useEffect(() => {
     const url = window.location.href;
-    const urls = url.split('/');
-    const event = urls.find((item) => item === 'event');
-    const news = urls.find((item) => item === 'news');
-    // console.log('event', event);
-    // console.log('news', news);
+    const urls = url.split("/");
+    const event = urls.find((item) => item === "event");
+    const news = urls.find((item) => item === "news");
     let data;
     if (event) {
       data = { queryParams: { eventId: params.id } };
       dispatch(triggerGetEventByID(data));
-      setDataType('event');
+      setDataType("event");
     } else if (news) {
       data = { queryParams: { newsId: params.id } };
       dispatch(triggerGetNewsByID(data));
-      setDataType('news');
+      setDataType("news");
     }
   }, []);
-  // console.log('datatype', dataType);
   return (
-    <div className='event-wrapper'>
-      {dataType === 'event' && (
+    <div className="event-wrapper">
+      {dataType === "event" && (
         <>
           <Banner tab={tab} />
           <EventDetails tab={tab} setTab={setTab} />
         </>
       )}
-      {dataType === 'news' && (
+      {dataType === "news" && (
         <>
           <Banner2 tab={tab} />
           <EventDetails2 tab={tab} setTab={setTab} />
