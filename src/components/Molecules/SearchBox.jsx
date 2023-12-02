c285d1c0f1a8338071dbb564adff211920160b8a
Chidiebere95 <chidiezeokwelume@gmail.com>
2023-12-02 18:54:45 +0100

    add search modal for forums

--- src/components/Molecules/SearchBox.jsx
+++ src/components/Molecules/SearchBox.jsx
@@ -1,14 +1,28 @@
-import React, { useRef } from "react";
-import "../../assets/scss/molecules.scss";
-import Icon from "../Icon";
+import React, { useRef } from 'react';
+import '../../assets/scss/molecules.scss';
+import Icon from '../Icon';
 
-const SearchBox = ({ onChange, value, placeholder, enterKeyPressed, otherKeysPressed }) => {
+const SearchBox = ({
+  onChange,
+  value,
+  placeholder,
+  height,
+  enterKeyPressed,
+  otherKeysPressed,
+}) => {
   const inputContainer = useRef(null);
 
   return (
-    <div className="search-box-component">
-      <input type="text" placeholder={placeholder} onChange={onChange} value={value} ref={inputContainer} />
-      <Icon icon="searchIcon" />
+    <div className='search-box-component'>
+      <input
+        type='text'
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        ref={inputContainer}
+        style={{ height }}
+      />
+      <Icon icon='searchIcon' />
     </div>
   );
 };
