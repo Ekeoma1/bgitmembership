80c36bf02e57cce544858ad4f36c691b0f66530d
ekeoma1 <ekeigwee@gmail.com>
2023-12-12 20:59:49 +0100

    update on event

--- src/components/Events-And-News/EventCard.jsx
+++ src/components/Events-And-News/EventCard.jsx
@@ -1,44 +1,34 @@
-import React from 'react';
-import { BsBookmark } from 'react-icons/bs';
-import { PiShareFat } from 'react-icons/pi';
-import { useNavigate } from 'react-router-dom';
-import Icon from '../Icon';
-import newsImg from '../../assets/images/news1.svg';
+import React from "react";
+import { BsBookmark } from "react-icons/bs";
+import { PiShareFat } from "react-icons/pi";
+import { useNavigate } from "react-router-dom";
+import Icon from "../Icon";
+import newsImg from "../../assets/images/news1.svg";
 
 const EventCard = ({ event, news, item }) => {
   const navigate = useNavigate();
   return (
     <div
-      className='event-card shadow-lg'
-      onClick={() =>
-        navigate(
-          news
-            ? `/events-and-news/news/${item.newsId}`
-            : `/events-and-news/event/${item.eventId}`
-        )
-      }
+      className="event-card shadow"
+      onClick={() => navigate(news ? `/events-and-news/news/${item.newsId}` : `/events-and-news/event/${item.eventId}`)}
     >
-      <div className='card-main'>
-        <div className='card-info'>
-          <h2 className='mt-lg-0 mt-3'>{item?.title}</h2>
-          <p className='date'>{item?.date}</p>
-          {event?.location && <p className='location'>{item?.location}</p>}
-          <p className='info'>{item?.content}</p>
+      <div className="card-main">
+        <div className="card-info">
+          <h2 className="mt-lg-0 mt-3">{item?.title}</h2>
+          <p className="date">{item?.date}</p>
+          {event?.location && <p className="location">{item?.location}</p>}
+          <p className="info">{item?.content}</p>
         </div>
-        <div className='card-img mt-lg-5 m-0'>
-          <img
-            src={item?.imageUrl || news ? newsImg : newsImg}
-            alt='event-people'
-            className='img-card'
-          />
+        <div className="card-img mt-lg-5 m-0">
+          <img src={item?.imageUrl || news ? newsImg : newsImg} alt="event-people" className="img-card" />
         </div>
       </div>
-      <div className='card-footer'>
-        <div className='icon-wrapper'>
-          <Icon icon='bookmark' />
+      <div className="card-footer">
+        <div className="icon-wrapper">
+          <Icon icon="bookmark" />
         </div>
-        <div className='icon-wrapper'>
-          <Icon icon='share' />
+        <div className="icon-wrapper">
+          <Icon icon="share" />
         </div>
       </div>
     </div>
